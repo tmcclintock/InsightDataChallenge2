4b64e0dbe6ba1473da939319016000fec6a38c87
Tom McClintock <tmcclintock89@gmail.com>
2020-02-17 13:57:52 -0500

    Wrote the setup file.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name = "InsightDataChallange2",
+      author = "Tom McClintock",
+      author_email = "thmsmcclintock@gmail.com",
+      url = "https://github.com/tmcclintock/InsightDataChallenge2",
+      packages = ["InsightDC2"]
+)
